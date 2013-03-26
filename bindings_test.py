52555cc8fb06cd8ef4f4c6ddcdb309dc56755744
= <asteed@soleranetworks.com>
2013-03-26 14:05:25 -0600

    ?

--- bindings_test.py
+++
@@ -1,9 +0,0 @@
-#MAEC v1.1 Bindings Test
-#Iterates through each Behavior in a Bundle and prints the Description Text
-
-import maecv11 as maec_binding
-
-maec_bundle = maec_binding.parse('analysis.xml')
-
-for behavior in maec_bundle.Behaviors.Behavior:
-    print behavior.Description.Text
